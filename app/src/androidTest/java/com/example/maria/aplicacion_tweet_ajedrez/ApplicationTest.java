810b4c45177044ea9fa55e42cf29b427bc33a0cf
mariano <marianoguido@yahoo.com.ar>
2016-08-11 06:16:34 -0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/maria/aplicacion_tweet_ajedrez/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.maria.aplicacion_tweet_ajedrez;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
